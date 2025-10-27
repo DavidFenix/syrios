5a7970d7d547cce16739a0cc2de42f95db5902d5
David Costa <deividesantoscosta@gmail.com>
2025-10-27 12:47:33 -0300

    inclusao do use Facades URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; 
 use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
 
